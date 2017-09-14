7415a18666969d1a7eee201459f1e7410420ce9c
BodoBurger <bodo.burger@gmail.com>
2017-09-14 17:59:12 +0200

    Creating URLs by dict comprehension

--- data/data_allensbach.py
+++ data/data_allensbach.py
@@ -7,19 +7,19 @@ Created on Fri Aug  4 22:37:28 2017
 #%%
 import pandas as pd
 from data.fetch_and_clean import get_table, cleaning_table
-from data.urls_sonntagsfrage import URL_ALLENSBACH
+from data.urls_sonntagsfrage import SONNTAGSFRAGE_URL
 
 #%% cleaning 2017
-table = get_table(URL_ALLENSBACH['2017'])
+table = get_table(SONNTAGSFRAGE_URL['allensbach']['2017'])
 table_clean_2017 = cleaning_table(table)
 
 #%% cleaning 2013
-table = get_table(URL_ALLENSBACH['2013'])
+table = get_table(SONNTAGSFRAGE_URL['allensbach']['2013'])
 table.loc[10, 'Befragte'] = '1500'
 table_clean_2013 = cleaning_table(table)
 
 #%% cleaning 2009
-table = get_table(URL_ALLENSBACH['2009'])
+table = get_table(SONNTAGSFRAGE_URL['allensbach']['2009'])
 table_clean_2009 = cleaning_table(table)
 
 
--- data/data_emnid.py
+++ data/data_emnid.py
@@ -7,18 +7,18 @@ Created on Fri Aug  4 22:37:28 2017
 #%%
 import pandas as pd
 from data.fetch_and_clean import get_table, cleaning_table
-from data.urls_sonntagsfrage import URL_EMNID
+from data.urls_sonntagsfrage import SONNTAGSFRAGE_URL
 
 #%% cleaning 2017
-table = get_table(URL_EMNID['2017'])
+table = get_table(SONNTAGSFRAGE_URL['emnid']['2017'])
 table_clean_2017 = cleaning_table(table)
 
 #%% cleaning 2013
-table = get_table(URL_EMNID['2013'])
+table = get_table(SONNTAGSFRAGE_URL['emnid']['2013'])
 table_clean_2013 = cleaning_table(table)
 
 #%% cleaning 2008
-table = get_table(URL_EMNID['2008'])
+table = get_table(SONNTAGSFRAGE_URL['emnid']['2008'])
 table_clean_2008 = cleaning_table(table)
 
 #%%
